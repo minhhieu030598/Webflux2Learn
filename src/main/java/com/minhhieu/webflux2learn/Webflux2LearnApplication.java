a18e7ce43b3ac40c32564ac0842051ffcc6d58c8
hieudm <minhhieudo03051998@gmail.com>
2024-03-13 18:21:22 +0700

    init commit

---
+++ src/main/java/com/minhhieu/webflux2learn/Webflux2LearnApplication.java
@@ -0,0 +1,13 @@
+package com.minhhieu.webflux2learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webflux2LearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webflux2LearnApplication.class, args);
+	}
+
+}
